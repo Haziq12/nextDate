f4abbb09fc2e4b6b88e4b41bdef0b805341640d0
Jayden Pak <jppak86@gmail.com>
2022-03-29 16:18:42 -0400

    delete-datePlan

--- src/pages/DatePlan/DatePlan.jsx
+++ src/pages/DatePlan/DatePlan.jsx
@@ -5,14 +5,14 @@ const DatePlan = ({datePlan, handleDeleteDatePlan, user, ownerEmail, ownerId}) =
   return (
     <>
       <div>
+        <Link to={`/dateplans/${datePlan._id}`} state={{ ownerId }}>
         <h2>Title:{datePlan.title}</h2>
         <h3>Location:{datePlan.location}</h3>
         <h3>Activity:{datePlan.activity}</h3>
         <h3>Food:{datePlan.food}</h3>
         <h3>Detail:{datePlan.detail}</h3>
-        {/* <h3>Chats:{datePlan.chats}</h3> */}
-        <Link to={`/dateplans/${datePlan._id}`} state={{ ownerId }}>
-              Show Date Plan Detail
+        
+              
             </Link>
         {ownerEmail[0] === user.email ? (
           <>
